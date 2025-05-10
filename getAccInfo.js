a1d184376fd411567a8455a1083ae9151ce2eb8c
913yum <89330535+yummy913@users.noreply.github.com>
2025-05-10 09:26:17 -0700

    Update getAccInfo.js

--- getAccInfo.js
+++ getAccInfo.js
@@ -5,7 +5,7 @@ async function getAccountInfo(walletAddress, updateUI) {
     const payload = { walletAddress };
 
     try {
-        const response = await fetch('http://localhost:3000/api/getAccountInfo', {
+        const response = await fetch('https://zkcomp-viewer-production.up.railway.app/api/getAccountInfo', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload)

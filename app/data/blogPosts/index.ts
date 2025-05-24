dbeeef0b531e93f8413d1703f708c2f33f8be8ae
913yum <89330535+yummy913@users.noreply.github.com>
2025-05-24 12:54:54 -0700

    Update index.ts

--- app/data/blogPosts/index.ts
+++ app/data/blogPosts/index.ts
@@ -1,1 +1,11 @@
-import type { BlogPost } from './compressed-nfts-guide'; import CompressedNFTsGuide from './compressed-nfts-guide'; import ZkCompressionGuide from './zk-compression-guide';   export type { BlogPost };  export const blogPosts: BlogPost[] = [   CompressedNFTsGuide,   ZkCompressionGuide ]; 
+import type { BlogPost } from './compressed-nfts-guide';
+import CompressedNFTsGuide from './compressed-nfts-guide';
+import ZkCompressionGuide from './zk-compression-guide';
+
+
+export type { BlogPost };
+
+export const blogPosts: BlogPost[] = [
+  CompressedNFTsGuide,
+  ZkCompressionGuide
+]; 

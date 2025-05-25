95f9f8037a05cfb28d89ece0c6460f766955ad05
913yum <89330535+yummy913@users.noreply.github.com>
2025-05-24 20:10:31 -0700

    update blog

--- app/data/blogPosts/index.ts
+++ app/data/blogPosts/index.ts
@@ -1,4 +1,5 @@
 import type { BlogPost } from '../../types/blog';
+import ReducingGasFeesOnSolanaWithZkCompression from './reducing-gas-fees-on-solana-with-zk-compression';
 import CompressedNFTsGuide from './compressed-nfts-guide';
 import ZkCompressionGuide from './zk-compression-guide';
 
@@ -7,5 +8,6 @@ export type { BlogPost };
 
 export const blogPosts: BlogPost[] = [
   CompressedNFTsGuide,
-  ZkCompressionGuide
+  ZkCompressionGuide,
+  ReducingGasFeesOnSolanaWithZkCompression
 ]; 

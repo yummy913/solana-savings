2b293b69e5370ee97b80c594111c044d54a42964
913yum <89330535+yummy913@users.noreply.github.com>
2025-05-24 20:13:33 -0700

    blog

---
+++ app/data/blogPosts/reducing-gas-fees-on-solana-with-zk-compression.tsx
@@ -0,0 +1,23 @@
+import '/app/styles/blogPost.css';
+import type { BlogPost } from '../../types/blog';
+
+const ReducingGasFeesonSolanawithzkCompression: BlogPost = {
+  id: '1',
+  title: 'Reducing Gas Fees on Solana with zk Compression',
+  date: '2025-05-25',
+  slug: 'reducing-gas-fees-on-solana-with-zk-compression',
+  customStyles: {
+    titleSize: '2.5rem',
+    paragraphSize: '1.1rem',
+    headingSize: '1.8rem',
+    backgroundColor: 'transparent',
+    textColor: 'rgba(255, 255, 255, 0.9)'
+  },
+  content: (
+    <>
+undefined
+    </>
+  )
+};
+
+export default ReducingGasFeesonSolanawithzkCompression;

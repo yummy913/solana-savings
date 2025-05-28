9e08ccb62389290ebd130be1cdd76b2363807165
913yum <89330535+yummy913@users.noreply.github.com>
2025-05-27 20:57:03 -0700

    update blog

--- app/data/blogPosts/index.ts
+++ app/data/blogPosts/index.ts
@@ -3,6 +3,7 @@ import CompressedNFTsGuide from './compressed-nfts-guide';
 import ZkCompressionGuide from './zk-compression-guide';
 import zkCompressionvsTraditionalCompressionWhichisBetterforSolana from './zk-compression-vs-traditional-compression-which-is-better-for-solana';
 import HowDevelopersAreSavingThousandsonSolanaWithZKCompression from './how-developers-are-saving-thousands-on-solana-with-zk-compression';
+import ZKCompressionExplainedLikeYoure5ButonSolana from './zk-compression-explained-like-youre-5-but-on-solana';
 
 
 export type { BlogPost };
@@ -11,5 +12,6 @@ export const blogPosts: BlogPost[] = [
   CompressedNFTsGuide,
   ZkCompressionGuide,
   zkCompressionvsTraditionalCompressionWhichisBetterforSolana,
-  HowDevelopersAreSavingThousandsonSolanaWithZKCompression
+  HowDevelopersAreSavingThousandsonSolanaWithZKCompression,
+  ZKCompressionExplainedLikeYoure5ButonSolana
 ]; 
